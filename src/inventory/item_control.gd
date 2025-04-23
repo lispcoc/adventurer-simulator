18d0773aa4269033a1d6da708e1e5194c444ec51
LISPCoC <fourier0520@gmail.com>
2025-04-24 00:32:22 +0900

    update

--- src/inventory/item_control.gd
+++ src/inventory/item_control.gd
@@ -1,4 +1,10 @@
 extends CtrlInventoryItemBase
 
 func _ready() -> void:
-	$ItemControlButton.pressed.connect(func(): self.grab_click_focus())
+	get_tree().call_group("InventoryMenu", "_on_item_entered", self)
+	$ItemControlButton.focus_entered.connect(func (): focus_entered.emit())
+
+func _get_drag_data(_at_position: Vector2) -> Variant:
+	return null
+	print(item)
+	return item
--- src/inventory/test.gd
+++ src/inventory/test.gd
@@ -1,18 +1,36 @@
-extends Node2D
+extends Control
 
 var iv : Inventory
-var grid : CtrlInventory
+var grid : CtrlInventoryGrid
 
 func _ready() -> void:
 	iv = $Inventory
-	grid = $CanvasLayer/CtrlInventory
-	grid.item_activated.connect(func(i): grid.select(i, false))
+	grid = $CanvasLayer/CenterContainer/CtrlInventoryGrid
 	for a in Game.get_party():
 		var it = InventoryItem.new()
 		it.set_property("name", a.actor_name)
 		it.set_property("actor", a)
 		iv.add_item(it)
 	grid.inventory_item_selected.connect(on_selected)
+	search_item.call_deferred()
+	grid.child_entered_tree.connect(func (n): print("aaaaaaaaa"))
+
+func _on_item_entered(item : CtrlInventoryItemBase):
+	item.focus_entered.connect(func (): print("focus_entered:" + item.name))
+
+func search_item():
+	for i in get_tree().get_nodes_in_group("ItemButton"):
+		print(i)
 
 func on_selected(item: InventoryItem):
-	print(item)
+	print("select:" )
+
+func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
+	return false
+	return data is InventoryItem
+
+
+func _drop_data(_at_position: Vector2, data: Variant) -> void:
+	return
+	grid.inventory.remove_item(data)
+	# Add custom logic for handling the item drop here
